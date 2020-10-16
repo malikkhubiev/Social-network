cfe1d7309f29ce0856cb6e7c6f0ee251bb4a24ed
malikkhubiev <malik.hubiev@mail.ru>
2020-10-16 15:03:06 +0300

    DID mount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,17 +3,19 @@ import styles from './Users.module.css';
 import UserItem from './UsersItem/UserItem';
 import * as axios from 'axios';
 
-const Users = (props) => {
-    if(props.users.length === 0){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
-            props.setUsers(response.data.items);
+class Users extends React.Component {
+    componentDidMount(){
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
         });
     }
-    return (
-        <div className={styles.sectionUsers}>
-            {props.users.map(asd => <UserItem follow={props.follow} unFollow={props.unFollow} key={asd.id} id={asd.id} name={asd.name} status={asd.status} place={asd.place} followed={asd.followed} />)}
-        </div>
-    )
+    render() {
+        return (
+            <div className={styles.sectionUsers}>
+                {this.props.users.map(asd => <UserItem follow={this.props.follow} unFollow={this.props.unFollow} key={asd.id} id={asd.id} name={asd.name} status={asd.status} place={asd.place} followed={asd.followed} />)}
+            </div>
+        )
+    }
 }
 
 export default Users;
